65a7f04fbc88efd083615e2a658bd1f732888812
qendev <qendev00@gmail.com>
2020-09-17 10:03:47 +0300

    converted the project to kotlin

---
+++ app/src/main/java/fragments/NotificationFragment.kt
@@ -0,0 +1,16 @@
+package fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bottomnavigation.R
+
+class NotificationFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
+    }
+}
